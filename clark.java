93a210cd358ed795bd4144561477868b6424f072
Clark Rodriguez <clarklrodriguez07@gmail.com>
2026-07-20 00:26:36 +0800

    initial commit

--- clark.java
+++ clark.java
@@ -2,14 +2,17 @@ import java.util.*;
 class clark{
     public static Scanner in=new Scanner(System.in);
     public static void main(String args[]){
-        int n=in.nextInt();
-        int a[]=new int[n];
-        for(int i=0;i<n;i++){
-            a[i]=in.nextInt();
-        }
-        Arrays.sort(a);
-        for(int i=0;i<n;i++){
-            System.out.print(a[i]+" ");
+        int num;
+
+        for(int i=0;i<5;i++){
+            System.out.println("Enter a number");
+            num=in.nextInt();
+            if(num%2==0){
+                System.out.println("Even");
+            }
+            else{
+                System.out.println("Odd");
+            }
         }
     }
 
