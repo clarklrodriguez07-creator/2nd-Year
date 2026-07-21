e9ac6cb705d6b1fb173a667cd4a943dd5bc0946b
Clark Rodriguez <clarklrodriguez07@gmail.com>
2026-07-21 15:12:32 +0800

    initial commit

--- clark.java
+++ clark.java
@@ -3,17 +3,32 @@ class clark{
     public static Scanner in=new Scanner(System.in);
     public static void main(String args[]){
         int num;
+        int choice=0;
 
-        for(int i=0;i<5;i++){
-            System.out.print("Enter a number: ");
-            num=in.nextInt();
-            if(num%2==0){
-                System.out.println("Number "+ num+" is Even");
-            }
-            else{
-                System.out.println("Number "+ num+" is Odd");
+        do{
+            System.out.println("Menu:");
+            System.out.println("1. ADD Number");
+            System.out.println("2. Exit");
+            System.out.print("Choice: ");
+            choice = in.nextInt();
+        if(choice==1){
+            for(int i=0;i<5;i++){
+                System.out.print("Enter a number: ");
+                num=in.nextInt();
+                if(num%2==0){
+                    System.out.println("Number "+ num+" is Even");
+                }
+                else{
+                    System.out.println("Number "+ num+" is Odd");
+                }   
             }
         }
-    }
-
+        else if(choice==2){
+            System.out.println("Exiting the program.");
+        }
+        else{
+            System.out.println("Invalid choice. Please try again.");
+        }
+    }while(choice!=2);
+  }
 }
