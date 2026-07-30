7bf0c04f9c593f6bec50a1a2abef59bc2d8d2aa2
Clark Rodriguez <clarklrodriguez07@gmail.com>
2026-07-30 22:46:27 +0800

    initial commit

--- entry.py
+++ entry.py
@@ -1,3 +1,15 @@
 print("Hello World!")
 
 print("After this Should learn basic python skills")
+
+if 10 > 5:
+    print("10 is greater than 5")
+
+print(3 + 7)
+print(2 * 5)
+print("I am", 19, "years old")
+
+x= 10
+y= 5    
+print(x)
+print(y)
