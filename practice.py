7c752abaca706848874cb54c4393e520f7560987
Clark Rodriguez <clarklrodriguez07@gmail.com>
2026-08-10 21:33:44 +0800

    initial commit

---
+++ practice.py
@@ -0,0 +1,11 @@
+print("Hellow Fellow Human!")
+name=input("What is your name? ")
+print("Hello " + name + "!")   
+age=input("How old are you? ")
+print("You are " + age + " years old.")
+job=input("What do you do for a living? ")
+print("So you are a " + job + ".")
+if job == "student":
+    print("You should focus on your studies.")
+else:
+    print("You should work hard to achieve your goals.")
