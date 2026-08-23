7207426591183282f0520c5541b3969e4fbc2a70
Clark Rodriguez <clarklrodriguez07@gmail.com>
2026-08-23 21:44:29 +0800

    initial commit

--- object.py
+++ object.py
@@ -1,3 +1,25 @@
+class Calculator:
+    
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2   
+    
+    def add(self):
+        return self.num1 + self.num2
+    def subtract(self):
+        return self.num1 - self.num2
+    def multiply(self):
+        return self.num1 * self.num2
+    def divide(self):
+        return self.num1 / self.num2
+    def exponentiate(self):
+        return self.num1 ** self.num2
+    def modulo(self):
+        return self.num1 % self.num2
+    def floor_divide(self):
+        return self.num1 // self.num2
+    
+
 while True:    
     print("=========================")   
     print("    SIMPLE CALCULATOR")  
@@ -23,19 +45,19 @@ while True:
         
 
         if choice == 1:
-            print(f"{num1} + {num2} = {num1 + num2}")
+            print(f"{num1} + {num2} = {Calculator(num1, num2).add()}")
         elif choice == 2:
-            print(f"{num1} - {num2} = {num1 - num2}")
+            print(f"{num1} - {num2} = {Calculator(num1, num2).subtract()}")
         elif choice == 3:
-            print(f"{num1} * {num2} = {num1 * num2}")
+            print(f"{num1} * {num2} = {Calculator(num1, num2).multiply()}")
         elif choice == 4:
-            print(f"{num1} / {num2} = {num1 / num2}")
+            print(f"{num1} / {num2} = {Calculator(num1, num2).divide()}")
         elif choice == 5:
-            print(f"{num1} ** {num2} = {num1 ** num2}")
+            print(f"{num1} ** {num2} = {Calculator(num1, num2).exponentiate()}")
         elif choice == 6:
-            print(f"{num1} % {num2} = {num1 % num2}")
+            print(f"{num1} % {num2} = {Calculator(num1, num2).modulo()}")
         elif choice == 7:
-            print(f"{num1} // {num2} = {num1 // num2}")
+            print(f"{num1} // {num2} = {Calculator(num1, num2).floor_divide()   }")
 
     else:
         print("Invalid choice. Please try again.")
