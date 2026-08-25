8a5c6def437a066b75cbc225b4c5b25d260bb2bb
Clark Rodriguez <clarklrodriguez07@gmail.com>
2026-08-25 23:06:06 +0800

    initial commit

--- practice.py
+++ practice.py
@@ -1,5 +1,36 @@
-a = int(input("First number: "))
-b = int(input("Second number: "))
+emfla = 375
+unit = int(input("Emergency flashlight purchased: "))
 
-c = a + b
-print(c)
+if unit <= 30:
+    total = unit * emfla
+    discount = total * .10
+    discounted = total - discount
+    if discounted > 12000:
+        add = discounted * .05
+        final = discounted - add
+    else:
+        add = 0
+        final = discounted
+
+    if final > 15000:
+        vat = final * .12
+    else:
+        vat = final * .08
+
+    final = final + vat       
+
+else:
+    total = unit * emfla
+    discount = total * .10
+    discounted = total - discount
+    add = 0
+    final = discounted
+    vat = final * .08
+    final = final + vat
+
+
+print("Total:", total)
+print("Discount:", discount)
+print("Additional Discount:", add)
+print("VAT:", vat)
+print("Final Price:", final)
